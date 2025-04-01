02030fe357a31fa94c6cb2259bb4e07ffc7b9b82
Gabriel <geugnio12@gmail.com>
2025-04-01 05:59:45 -0300

    feat(supplier): endpoint de remoção de fornecedores por ID

--- src/main/java/com/gabrielestudo/sistema_de_vendas/controllers/SupplierController.java
+++ src/main/java/com/gabrielestudo/sistema_de_vendas/controllers/SupplierController.java
@@ -3,14 +3,17 @@ package com.gabrielestudo.sistema_de_vendas.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.gabrielestudo.sistema_de_vendas.dtos.SupplierCreateDTO;
 import com.gabrielestudo.sistema_de_vendas.services.SupplierService;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/supplier")
@@ -32,9 +35,18 @@ public class SupplierController {
     }
 
     @PostMapping("/add")
-    public String addSupplier(@Validated @ModelAttribute SupplierCreateDTO data, Model model) {
+    public String addSupplier(@Valid @ModelAttribute SupplierCreateDTO data, Model model) {
         supplierService.save(data);
         return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String removeSupplier(@PathVariable Long id) {
+        if (!supplierService.existsById(id)) {
+            throw new EntityNotFoundException("Supplier with id " + id + " not found");
+        }
+        supplierService.deleteById(id);
+        return "redirect:/supplier/list";
     }
 
 }
