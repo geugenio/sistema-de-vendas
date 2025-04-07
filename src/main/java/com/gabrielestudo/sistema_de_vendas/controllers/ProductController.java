b8cd3474762819514c932319ebd84194908d8492
Gabriel <geugnio12@gmail.com>
2025-04-07 06:46:37 -0300

    feat(delete product): adicionado botão para remoção de produto usando metodo delete, bem como seu estilo.

--- src/main/java/com/gabrielestudo/sistema_de_vendas/controllers/ProductController.java
+++ src/main/java/com/gabrielestudo/sistema_de_vendas/controllers/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -58,7 +59,7 @@ public class ProductController {
         return "redirect:/product/list";
     }
 
-    @GetMapping("/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public String removeSupplier(@PathVariable("id") Long id) {
         if (!productService.existsById(id)) {
             throw new EntityNotFoundException("Product with id " + id + " not found");
