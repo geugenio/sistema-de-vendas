2afb39a9f533b97fa4e4665df80295029e04af74
Gabriel <geugnio12@gmail.com>
2025-04-11 05:23:10 -0300

    feat(list style): ajuste no estilo das listas e adição de botão de remoção de fornecedores

--- src/main/java/com/gabrielestudo/sistema_de_vendas/controllers/SupplierController.java
+++ src/main/java/com/gabrielestudo/sistema_de_vendas/controllers/SupplierController.java
@@ -3,6 +3,7 @@ package com.gabrielestudo.sistema_de_vendas.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,7 +43,7 @@ public class SupplierController {
         return "redirect:/";
     }
 
-    @GetMapping("/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public String removeSupplier(@PathVariable Long id) {
         if (!supplierService.existsById(id)) {
             throw new EntityNotFoundException("Supplier with id " + id + " not found");
